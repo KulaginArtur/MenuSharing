8bd81e061b182849dc9303ff01eb3a6e34660f45
KulaginArtur <artur.kulagin@gmail.com>
2019-05-06 10:44:21 +0300

    commit

--- script.js
+++ script.js
@@ -34,5 +34,94 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 document.getElementById("defaultOpen").click();
+
+// by default all "meals" are hidden
+
+const meal_mean = document.getElementsByClassName("meal-mean");
+
+for(let i=0;i<meal_mean.length;i++){
+  meal_mean[i].style.display = "none"
+};
+const meal_app = document.getElementsByClassName("meal-app");
+for(let i=0;i<meal_app.length;i++){
+  meal_app[i].style.display = "none"
+};
+const meal_des = document.getElementsByClassName("meal-des");
+for(let i=0;i<meal_des.length;i++){
+  meal_des[i].style.display = "none"
+};
+
+const meal_drinks = document.getElementsByClassName("meal-drinks");
+for(let i=0;i<meal_drinks.length;i++){
+  meal_drinks[i].style.display = "none"
+};
+
+//ends "hiding meals" block
+
+//if user clicks "typeOfFood" (for example, Mains or Appetizers etc.) then all meals in this category are shown
+
+const mains = document.getElementById("mains");
+const appetizers = document.getElementById("appetizers");
+const deserts = document.getElementById("deserts");
+const drinks = document.getElementById("drinks");
+const span = document.getElementById('icon1');
+const span1 = document.getElementById('icon2');
+const span2 = document.getElementById('icon3');
+const span3 = document.getElementById('icon4');
+
+// "show meals" block for "Mains"
+
+mains.addEventListener('click', () => {
+
+  for (let i = 0; i < document.getElementsByClassName('meal-mean').length; i++) {
+    console.log(document.getElementsByClassName('meal-mean')[i]);
+    if (document.getElementsByClassName('meal-mean')[i].style.display === "none"){
+      document.getElementsByClassName('meal-mean')[i].style.display = "block";
+      span.innerText = "▽";}
+    else {document.getElementsByClassName('meal-mean')[i].style.display = "none";
+      span.innerText = "▷"; };
+  };
+});
+
+// "show meals" block for "Appetizers"
+
+appetizers.addEventListener('click', () => {
+
+  for (let i = 0; i < document.getElementsByClassName('meal-app').length; i++) {
+    console.log(document.getElementsByClassName('meal-app')[i]);
+    if (document.getElementsByClassName('meal-app')[i].style.display === "none"){
+      document.getElementsByClassName('meal-app')[i].style.display = "block";
+      span1.innerText = "▽";}
+    else {document.getElementsByClassName('meal-app')[i].style.display = "none";
+      span1.innerText = "▷"; };
+  };
+});
+
+// "show meals" block for "Desserts"
+
+deserts.addEventListener('click', () => {
+
+  for (let i = 0; i < document.getElementsByClassName('meal-des').length; i++) {
+    console.log(document.getElementsByClassName('meal-des')[i]);
+    if (document.getElementsByClassName('meal-des')[i].style.display === "none"){
+      document.getElementsByClassName('meal-des')[i].style.display = "block";
+      span2.innerText = "▽";}
+    else {document.getElementsByClassName('meal-des')[i].style.display = "none";
+      span2.innerText = "▷"; };
+  };
+});
+
+// "show meals" block for "Drinks"
+
+deserts.addEventListener('click', () => {
+
+  for (let i = 0; i < document.getElementsByClassName('meal-drinks').length; i++) {
+    console.log(document.getElementsByClassName('meal-drinks')[i]);
+    if (document.getElementsByClassName('meal-drinks')[i].style.display === "none"){
+      document.getElementsByClassName('meal-drinks')[i].style.display = "block";
+      span3.innerText = "▽";}
+    else {document.getElementsByClassName('meal-drinks')[i].style.display = "none";
+      span3.innerText = "▷"; };
+  };
+});
